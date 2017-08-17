b9b161f271a0eab3db45e840c3dc5fd580fc1545
Mupati <scientificgh@gmail.com>
2017-08-17 16:05:34 +0000

    Handles exceptions due to blocked routes

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -60,6 +60,16 @@ class Handler extends ExceptionHandler
             return response()->json(['error' => 'Unauthenticated.'], 401);
         }
 
-        return redirect()->guest(route('login'));
+        $guard = array_get($exception->guards(), 0);
+            switch ($guard) {
+                case 'membership':
+                    return redirect()->guest(route('registrationForm'));         break;
+                default:
+                    return redirect()->guest(route('login'));
+                        break;
+            }
     }
+
+
+
 }
